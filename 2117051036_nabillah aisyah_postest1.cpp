a6c7aa01796094aeb829a9335b266347ac6f35d8
Nabillah Aisyah <92416388+nabillahaisyah@users.noreply.github.com>
2021-11-12 21:22:05 +0700

    Add files via upload

---
+++ 2117051036_nabillah aisyah_postest1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+	//input
+	
+	string nama;
+	string npm;
+	string alamat;
+	string emot;
+	
+	cout<<"Masukkan Namamu =";
+	getline (cin,nama);
+	cout<<"Masukkan NPM = ";
+	cin>>npm;
+	cout<<"Masukkan Alamat = ";
+	cin>>alamat;
+	
+	//output
+	
+	cout<<" Nama saya adalah " <<nama<< " dengan npm " <<npm<< " dan beralamat di " <<alamat<<endl;
+    
+    cout<<">//<"<<emot<<endl;
+    cout<<"Nabillah Aisyah";
+    
+	return 0;	
+}
